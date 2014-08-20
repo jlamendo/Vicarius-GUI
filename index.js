1f7de0740d00e44e6449b6b3a5669d708681b5b0
Jon <jonl@andyet.net>
2014-08-19 17:36:55 -0700

    added callback to start function

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var Hapi = require('hapi');
 var config = require('getconfig');
 var moonbootsConfig = require('./moonbootsConfig');
 
-module.exports = function(opts){
+module.exports = function(opts, cb){
 var server = new Hapi.Server(opts.GUI.host, opts.GUI.port);
 // require moonboots_hapi plugin
 server.pack.require({'moonboots_hapi': moonbootsConfig}, function (err) {
     if (err) throw err;
         server.start(function (err) {
             if (err) throw err;
-            console.log("Vicarius is running at: http://" + opts.GUI.host + ":" + opts.GUI.port);
+            cb();
         });
 return server;
 });
